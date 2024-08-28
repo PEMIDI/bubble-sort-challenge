4cd0c5c402f5486ae84f1e9a636babb8c579d5e5
pemidi <rashidi.peyman@gmail.com>
2024-08-28 23:57:55 +0330

    solve the problem

--- main.py
+++ main.py
@@ -1,32 +1,18 @@
-# TODO : 152.10.3.5, 0.0.42, 36, 152.11, 0.0.42.1
-
-
-
-
-# 0 < n <= 50
-
 versions = ['152.10.3.5', '0.0.42', '36', '152.11', '0.0.42.1']
 
+versions_list_length = len(versions)
 
-k = len(versions)
+for i in range(versions_list_length):
+    for j in range(versions_list_length - i - 1):
+        element_one = versions[j].split('.')
+        element_two = versions[j + 1].split('.')
 
-for i in range(k):
+        for index in range(len(element_one)):
+            element_one[index] = int(element_one[index])
+        for index in range(len(element_two)):
+            element_two[index] = int(element_two[index])
 
-    for j in range(k-i):
+        if element_one > element_two:
+            versions[j], versions[j + 1] = versions[j + 1], versions[j]
 
-        for item in
-        if versions[j].split('.') > versions[j+1].split('.'):
-            versions[j], versions[j+1] = versions[j+1], versions[j]
-
-
-
-
-
-
-
-
-
-
-
-
-
+print(versions)
